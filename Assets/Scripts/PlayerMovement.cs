c23fbfc0826c5fb5deaf1f9e2e9b8a6b668a8f09
BorDekker <32562@ma-web.nl>
2025-04-09 11:11:21 +0200

    Created a jump for the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
@@ -9,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _sprintAction;
     private InputAction _lookAction;
+    private InputAction _jumpAction;
 
     private CharacterController _playerController;
     private Vector3 _velocity;
@@ -16,8 +19,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int speed;
     [SerializeField] private int gravity;
     [SerializeField] private int sprintMult;
+    [SerializeField] private int jumpSpeed;
 
     [SerializeField] private float lookSens;
+
+    [SerializeField] private Vector3 groundCheck;
+    
+    [SerializeField] private float groundCheckSize;
+    private List<GameObject> _gameObjects;
+    [SerializeField] private LayerMask groundLayerMask;
    
     void Start()
     {
@@ -27,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
         _moveAction = _playerInputMap.FindAction("Move");
         _sprintAction = _playerInputMap.FindAction("Sprint");
         _lookAction = _playerInputMap.FindAction("Look");
+        _jumpAction = _playerInputMap.FindAction("Jump");
 
         _playerController = GetComponent<CharacterController>();
         
@@ -41,12 +52,27 @@ public class PlayerMovement : MonoBehaviour
         var move = transform.right * moveValue.x + transform.forward * moveValue.y;
         _playerController.Move( totalSpeed * Time.deltaTime * move);
         
-        
         Vector3 rot = transform.rotation.eulerAngles;
         rot = new Vector3(0, rot.y+lookValue.x, 0);
         transform.rotation = Quaternion.Euler(rot);
+
+        if (Physics.CheckSphere(transform.position - groundCheck, groundCheckSize, groundLayerMask))
+        {
+            _velocity.y = gravity;
+        }
         
+        float jumpValue = _jumpAction.ReadValue<float>();
+        if (jumpValue == 1 && Physics.CheckSphere(transform.position - groundCheck, groundCheckSize, groundLayerMask))
+        {
+            _velocity.y = Mathf.Sqrt(jumpValue * jumpSpeed * -2f * gravity);
+        }
         _velocity.y += gravity * Time.deltaTime;
         _playerController.Move(_velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position - groundCheck, groundCheckSize);
     }
 }
