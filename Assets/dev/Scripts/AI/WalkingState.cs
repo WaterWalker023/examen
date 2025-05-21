afd0e894c0ea43b0fbae0cba162553b9ed5b502b
Lysorian <105425047+Lysorian@users.noreply.github.com>
2025-05-21 15:55:44 +0200

    Rat animations

--- Assets/dev/Scripts/AI/WalkingState.cs
+++ Assets/dev/Scripts/AI/WalkingState.cs
@@ -9,26 +9,35 @@ public class WalkingState : MonoBehaviour
     private NavMeshAgent _agent;
     [SerializeField] private Vector4 randomrange;
     private Transform Starting;
+    private Animator RatController;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Starting = transform;
         _agent = gameObject.GetComponent<NavMeshAgent>();
+
+        RatController = GetComponentInChildren<Animator>();
+        RatController.SetBool("Walking", true);
     }
-    
+
+
     private void OnEnable()
     {
         _agent = gameObject.GetComponent<NavMeshAgent>();
         _agent.speed = 10;
+        RatController.SetBool("Walking", true);
     }
     // Update is called once per frame
     void Update()
     {
         if (_agent.remainingDistance <= 1)
         {
-            _agent.SetDestination(new Vector3((Random.Range(randomrange.x, randomrange.y + 1)+ Starting.position.x), 0,
+            _agent.SetDestination(new Vector3((Random.Range(randomrange.x, randomrange.y + 1) + Starting.position.x), 0,
                 (Random.Range(randomrange.z, randomrange.w + 1) + Starting.position.z)));
+                
+            
         }
     }
 }
